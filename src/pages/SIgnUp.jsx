4c424053b78ee3aef65ce2932b7e65c1d160b567
Jois Prabhanjan <prabhanjan.jois@cognitiveclouds.com>
2023-01-03 18:22:28 +0530

    made sign-in-up page responsive

--- src/pages/SIgnUp.jsx
+++ src/pages/SIgnUp.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
+import "./styles/sign-in-up.css";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logo_trainery.png";
 import Ellipse from "../assets/Ellipse5.png";
 import Image1 from "../assets/image1.png";
 import Union from "../assets/Union.png";
-import "./styles/sign-in-up.css";
-// eslint-disable-next-line no-unused-vars
-import SimpleImageSlider from "react-simple-image-slider";
-
-const SIgnUp = () => {
-  const history = useNavigate();
-  const home = () => {
-    history("/");
-  };
-
+const NewSignUp = () => {
   const [email, setEmail] = useState("");
   //const [password,setPassword]=useState("");
   // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(null);
   const [acceptEmail, setAcceptEmail] = useState(false);
-
+  const [showPassword, setShowPassword] = useState(false);
   const [hidePassword, setHidePassword] = useState(false);
+
+  //
+
+  const history = useNavigate();
+  const home = () => {
+    history("/");
+  };
 
   const togglePasswordVisiblity = () => {
     setHidePassword(!hidePassword);
@@ -41,100 +40,102 @@ const SIgnUp = () => {
     setEmail(event.target.value);
   };
 
-  const handleSubmit = () => {
+  const handleNext = () => {
     if (acceptEmail) {
       console.log("came here");
+      setShowPassword(true);
     }
   };
-  // eslint-disable-next-line no-unused-vars
-  const images = [
-    { url: Image1 },
-    { url: Image1 },
-    { url: Image1 },
-    { url: Image1 },
-    { url: Image1 },
-  ];
+  const handleSubmit = () => {};
   return (
-    <div>
-      <div className="mainContainer">
-        <img alt="" src={Logo} width="138.75" height="30" className="logo" />
-      </div>
-      <img
-        alt=""
-        src={Ellipse}
-        width="150"
-        height="150"
-        className="imgStyle2"
-      />
-      <div className="line"></div>
-      <div className="signIn_text">Sign up</div>
-      <div className="text-para">
-        Enter the below details and create your own account.
-      </div>
-      <input
-        placeholder="Orgnization Name"
-        className="input-col"
-        onChange={handleChange}
-        value={email}
-        style={{ top: "280px" }}
-      />
-      <input
-        placeholder="Email address or mobile number"
-        className="input-col"
-        onChange={handleChange}
-        value={email}
-        style={{ top: "352px" }}
-      />
-
-      <div>
-        <input
-          placeholder="Password"
-          className="input-col2"
-          type={hidePassword ? "text" : "password"}
-          style={{ top: "424px" }}
-        />
-        <div className="eyeIcon" style={{ top: "442px" }}>
-          <i
-            className="fa fa-eye"
-            aria-hidden="true"
-            onClick={togglePasswordVisiblity}
-          />
+    <div className="gg">
+      <div className="main-co">
+        <div className="cont">
+          <div className="left">
+            <img
+              alt=""
+              src={Logo}
+              width="138.75"
+              height="30"
+              className="logo1"
+            />
+            <div className="signIn_text1">Sign up</div>
+            <div className="text-para1">
+              Enter the below details and create your own account.
+            </div>
+            <input
+              placeholder="Orgnization Name"
+              className="input-col-page2"
+              onChange={handleChange}
+              value={email}
+              style={{}}
+            />
+            <input
+              placeholder="Email address or mobile number"
+              className="second-box"
+              onChange={handleChange}
+              value={email}
+            />
+            <div className="password-box">
+              <input
+                placeholder="Password"
+                className="input-col21"
+                type={hidePassword ? "text" : "password"}
+              />
+              <i
+                className="fa fa-eye"
+                aria-hidden="true"
+                onClick={togglePasswordVisiblity}
+              />
+            </div>
+            <button className="btn21" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+          <div className="right">
+            <img
+              alt=""
+              src={Union}
+              width="77.48px"
+              height="67px"
+              className="img31"
+            />
+            <img
+              alt=""
+              src={Ellipse}
+              width="150"
+              height="150"
+              className="img1"
+            />
+            <div className="para-text1">
+              Growing your people,
+              <br />
+              growing your company, together
+            </div>
+            <img
+              alt=""
+              src={Image1}
+              width="435.87px"
+              height="146.78px"
+              className="peopleImg"
+            />
+          </div>
         </div>
-
-        <button className="btn-submit" onClick={handleSubmit}>
-          Submit
-        </button>
       </div>
-
-      <div className="para-text">
-        Growing your people,
-        <br />
-        growing your company, together
-      </div>
-      <img
-        alt=""
-        src={Image1}
-        width="435.87px"
-        height="146.78px"
-        className="img2"
-      />
-      {/* <SimpleImageSlider
-        width={435.87}
-        height={146.78}
-        images={images}
-        showBullets={true}
-        showNavs={true}
-        className="img2"
-      /> */}
-      <div className="end-text">
+      <div className="end-text1">
         To go back and Login{" "}
         <span style={{ color: " #295BDC", cursor: "pointer" }} onClick={home}>
           Click here
         </span>
+        <button
+          style={{ marginLeft: "1rem" }}
+          onClick={() => history("/notifications")}
+        >
+          notification page
+        </button>
       </div>
-      <img alt="" src={Union} width="77.48px" height="67px" className="img3" />
     </div>
   );
 };
 
-export default SIgnUp;
+export default NewSignUp;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./styles/sign-in-up.css";
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logo_trainery.png";
 import Ellipse from "../assets/Ellipse5.png";
 import Image1 from "../assets/image1.png";
 import Union from "../assets/Union.png";
-const SignIn = () => {
+const Newsignin = () => {
   const [email, setEmail] = useState("");
   //const [password,setPassword]=useState("");
   // eslint-disable-next-line no-unused-vars
@@ -47,72 +47,89 @@ const SignIn = () => {
     }
   };
   const handleSubmit = () => {};
-
   return (
-    <div>
-      <div className="mainContainer">
-        <img alt="" src={Logo} width="138.75" height="30" className="logo" />
-      </div>
-      <img
-        alt=""
-        src={Ellipse}
-        width="150"
-        height="150"
-        className="imgStyle2"
-      />
-      <div className="line"></div>
-      <div className="signIn_text">Sign in</div>
-      <div className="text-para">
-        One Trainery account for Everything eLearning and HR Solutions
-      </div>
-      <input
-        placeholder="Email address or mobile number"
-        className="input-col"
-        onChange={handleChange}
-        value={email}
-      />
-      {!showPassword ? (
-        <>
-          <button className="btn" onClick={handleNext}>
-            Next
-          </button>
-          <div className="forgot-text">Forgot password?</div>
-        </>
-      ) : (
-        <div>
-          <input
-            placeholder="Password"
-            className="input-col2"
-            type={hidePassword ? "text" : "password"}
-          />
-          <div className="eyeIcon">
-            <i
-              className="fa fa-eye"
-              aria-hidden="true"
-              onClick={togglePasswordVisiblity}
+    <div className="gg">
+      <div className="main-co">
+        <div className="cont">
+          <div className="left">
+            <img
+              alt=""
+              src={Logo}
+              width="138.75"
+              height="30"
+              className="logo1"
+            />
+            <div className="signIn_text1">Sign in</div>
+            <div className="text-para1">
+              One Trainery account for Everything eLearning and HR Solutions
+            </div>
+            <input
+              placeholder="Email address or mobile number"
+              className="input-col1"
+              onChange={handleChange}
+              value={email}
+            />
+            {!showPassword ? (
+              <>
+                <button className="btn1" onClick={handleNext}>
+                  Next
+                </button>
+
+                <div className="forgot-text1">Forgot password?</div>
+              </>
+            ) : (
+              <div>
+                <div className="password-box">
+                  <input
+                    placeholder="Password"
+                    className="input-col21"
+                    type={hidePassword ? "text" : "password"}
+                  />
+                  <i
+                    className="fa fa-eye"
+                    aria-hidden="true"
+                    onClick={togglePasswordVisiblity}
+                  />
+                </div>
+
+                <button className="btn21" onClick={handleSubmit}>
+                  Next
+                </button>
+                <div className="forgot-text21">Forgot password?</div>
+              </div>
+            )}
+          </div>
+          <div className="right">
+            <img
+              alt=""
+              src={Union}
+              width="77.48px"
+              height="67px"
+              className="img31"
+            />
+            <img
+              alt=""
+              src={Ellipse}
+              width="150"
+              height="150"
+              className="img1"
+            />
+            <div className="para-text1">
+              Growing your people,
+              <br />
+              growing your company, together
+            </div>
+            <img
+              alt=""
+              src={Image1}
+              width="435.87px"
+              height="146.78px"
+              className="peopleImg"
             />
           </div>
-
-          <button className="btn2" onClick={handleSubmit}>
-            Next
-          </button>
-          <div className="forgot-text2">Forgot password?</div>
         </div>
-      )}
-
-      <div className="para-text">
-        Growing your people,
-        <br />
-        growing your company, together
       </div>
-      <img
-        alt=""
-        src={Image1}
-        width="435.87px"
-        height="146.78px"
-        className="img2"
-      />
-      <div className="end-text">
+      <div className="end-text1">
         Don't have an account?{" "}
         <span
           style={{ color: " #295BDC", cursor: "pointer" }}
@@ -120,13 +137,15 @@ const SignIn = () => {
         >
           Create account
         </span>
+        <button
+          style={{ marginLeft: "1rem" }}
+          onClick={() => history("/notifications")}
+        >
+          notification page
+        </button>
       </div>
-      <img alt="" src={Union} width="77.48px" height="67px" className="img3" />
-      <button onClick={() => history("/notifications")}>
-        notification page
-      </button>
     </div>
   );
 };
 
-export default SignIn;
+export default Newsignin;
